97deb7211c56544647337393747a98740f23e6b7
m-moiz <muhammadmoiz856@hotmail.com>
2023-01-16 14:35:36 -0800

    Added exception handling for API requests and cleaned startup class

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -10,12 +10,11 @@ using Core.Interfaces;
 using Core.Specifications;
 using API.Dtos;
 using AutoMapper;
+using API.Errors;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController: ControllerBase
+    public class ProductsController: BaseApiController
     {
         private readonly IGenericRepository<Product> _productRepo;
         private readonly IGenericRepository<ProductBrand> _productBrandRepo;
@@ -42,10 +41,13 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
             var spec = new ProductsWithTypesAndBrandsSpecification(id);
             var product = await _productRepo.GetEntityWithSpec(spec);
+            if(product == null) return NotFound(new ApiResponse(404));
             var productToReturn = _mapper.Map<Product, ProductToReturnDto>(product);
             return Ok(productToReturn);
             
